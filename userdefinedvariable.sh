25c2cc830eccd0031166d10435d16470ad346eac
Chonta <mendysimonpeter@gmail.com>
2022-08-24 04:37:45 +0000

    add script shell files.

---
+++ userdefinedvariable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash      
+#title           :
+#description     :
+#author		     :Simon Peter Mendy
+#date            :08112012
+#version         :1.0    
+#Tel		     :
+
+name="Simon Legah"
+id=08112012
+
+echo "The name varibale value is: "$name
+echo "The id variable value is: "$id
+
